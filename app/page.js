764b2a4c7b717e3b91b801c656c4efa9d968e864
Divine Agbasimelo <divinegodwin172@gmail.com>
2023-12-02 22:00:29 +0100

    headers and navs

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
-
 import styles from './page.module.css'
 
 export default function Home() {
-  
-  
+
+
+  return (
+    <div>
+
+      <img src="/image1.jpg" className='h-[530px]'></img>
+    
+
+      <div>
+        <p className='text-xl font-bold pl-4 pt-4'>Category</p>
+      </div>
+
+    </div>
+  )
+
+
+
 }
